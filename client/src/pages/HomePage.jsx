8047066265c6e5521168d8c6b019e009cafe7ef5
neeraj678singh <neerajsinghoime678@gmail.com>
2026-04-09 00:58:23 +0530

    Initial commit

---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,24 @@
+import React, {useState} from 'react'
+import SideBar from '../components/SideBar'
+import ChatContainer from '../components/ChatContainer'
+import RightSideBar from '../components/RightSideBar'
+
+const HomePage = () => {
+
+    const [selectUser, setSelectedUser] = useState(false);
+
+
+    return (
+        <div className=' border w-full h-screen sm:px-[15%] sm:py-[5%] '>
+            <div className={`backdrop-blur-x1 border-2 border-gray-600 rounded-2x1 overflow-hidden h-[100%] grid grid-cols-1 relative
+                ${selectUser ? 'md:grid-cols-[1fr_1.5fr_1fr] x1:grid-cols-[1fr_2fr_1fr]' : 'md:grid-cols-2'}                 
+                `}>
+                <SideBar selectedUser={selectUser} setSelectedUser={setSelectedUser} />
+                <ChatContainer selectedUser={selectUser} setSelectedUser={setSelectedUser} />
+                <RightSideBar selectedUser={selectUser} setSelectedUser={setSelectedUser} />
+            </div>
+        </div>
+    )
+}
+
+export default HomePage
